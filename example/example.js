e0404961bf659f97f7328204e064393f282020f7
Vanja Cosic <vanjacosic@gmail.com>
2014-10-30 20:53:54 +0100

    Added example site for testing

---
+++ example/example.js
@@ -0,0 +1,29 @@
+console.log('Example site loaded.');
+
+// Configure client
+Raven.config({
+    api_host: 'http://0.0.0.0:9000',
+    orgId: '21c3358438094c01acbb38554436497a',
+    appId: '53d206094c'
+}).install();
+
+// Set user data
+Raven.setUserContext({
+    email: 'vanja@cosic.dk',
+    id: 1
+})
+
+// Test functions
+function multiply(a, b) {
+    "use strict";
+    return a * b;
+}
+
+function divide(a, b) {
+    "use strict";
+    try {
+        return multiply(add(a, b), a, b) / c;
+    } catch (e) {
+        Raven.captureException(e);
+    }
+}
